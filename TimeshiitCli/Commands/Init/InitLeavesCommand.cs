d097db402d506b3162a94f030503309f29acf114
Martin Škuta <martin.skuta@outlook.com>
2026-03-03 20:34:44 +0100

    Fix leaves file path

--- TimeshiitCli/Commands/Init/InitLeavesCommand.cs
+++ TimeshiitCli/Commands/Init/InitLeavesCommand.cs
@@ -10,15 +10,16 @@ public sealed class InitLeavesCommand(DirectoryInfo folderOutput, int year) : Co
         await ZohoCli.RunCommand("auth logout");
         Console.WriteLine("Initializing Zoho login procedure...");
         await ZohoCli.RunCommand("auth login");
-        
+
         var fromDate = new DateOnly(year, 1, 1);
         var toDate = new DateOnly(year, 12, 31);
         const string dateFormat = "yyyy-MM-dd";
-        
-        Console.WriteLine("Initializing Zoho login procedure...");
-        var leaveInfosJson = await ZohoCli.RunCommand($"leave get all --fromDate {fromDate.ToStringInvariant(dateFormat)}  --toDate {toDate.ToStringInvariant(dateFormat)} --dateFormat {dateFormat}");
-        
-        var resultFilePath = folderOutput + $"leaves_{year}.json";
+
+        var leaveInfosJson =
+            await ZohoCli.RunCommand(
+                $"leave get all --fromDate {fromDate.ToStringInvariant(dateFormat)}  --toDate {toDate.ToStringInvariant(dateFormat)} --dateFormat {dateFormat}");
+
+        var resultFilePath = Path.Combine(folderOutput.FullName, $"leaves_{year}.json");
         await File.WriteAllTextAsync(resultFilePath, leaveInfosJson, cancellationToken);
         Console.WriteLine($"Created leaves file at {resultFilePath}");
     }
